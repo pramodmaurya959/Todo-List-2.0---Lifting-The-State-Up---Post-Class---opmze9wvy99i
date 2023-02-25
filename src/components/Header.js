4e01bd403fa44cfbca546e88da08e70f3867160a
pramodmaurya959 <118468618+pramodmaurya959@users.noreply.github.com>
2023-02-25 15:43:16 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,8 @@ import React from "react";
 import "../styles/App.css";
 
 const Header = () => {
-  return <h2 className="header">To-Do List</h2>;
+  return( <h2 className="header">To-Do List</h2>;
+        )
 };
 
 export default Header;
